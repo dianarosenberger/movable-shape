df7c3173d241ddadd3d0a9f3ea46fa305e38632b
dianarosenberger <fc4431@wayne.edu>
2016-02-11 21:36:09 -0500

    Vertical Moving Rocket
    Made rocket move vertically

--- sketch.js
+++ sketch.js
@@ -1,35 +1,29 @@
 
 // give movableShape a nice, descriptive name to work with here
 // (i.e., replace the variable name myShape with something more descriptive (e.g., myCar, myRocket).)
-var myCar = movableShape;
+var myRocket = movableShape;
 
 // Redefine drawShape() by putting your moving shape in here
 // (Basically, you can copy and paste your code from last time, although you may wish to refactor it so that it's more readable.)
 // That said, you may wish to try putting your unrefactored, raw drawing code from week 1 in here
-myCar.drawShape = function () {
-  ellipse(270, 460, 400, 100); // body of car
-	fill(0); // black
-  ellipse(160, 515, 60, 60); // back wheel
-  ellipse(375, 515, 60, 60); // front wheel
-	fill(300);//white
-  ellipse(160, 515, 30, 30);//back wheel white
-  ellipse(375, 515, 30, 30);//front wheel white
-  ellipse(200, 400, 50, 100);//back seat
-  ellipse(325, 400, 50, 100);//front seat
-	fill(0);//black steering wheel
-  ellipse(400, 415, 50, 50);//outer steering wheel
-	fill(300);//white
-  ellipse(400, 415, 30, 30);//inner steering wheel
+myRocket.drawShape = function () {
+fill(300); // make white
+triangle (75, 500, 250, 300, 450, 500); // bottom triangle
+fill(150); // make gray
+ellipse(250, 300, 221, 450); // oval
+fill(300); // make white
+triangle(150, 150, 350, 150, 250, 50); // first triangle
+ellipse(255, 300, 70, 70); // window
 };
 
 setup = function() {
 	createCanvas(600, 600);
-	myCar.speed.x *= 0.5;
+	myRocket.speed.y *= 3.08;
 
 };
 
 draw = function() {
   background(100); // refresh the background
-  myCar.display(); // display myShape
-  myCar.update(); // and then update it
+  myRocket.display(); // display myShape
+  myRocket.update(); // and then update it
 };
